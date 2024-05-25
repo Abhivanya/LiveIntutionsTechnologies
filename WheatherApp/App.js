a841702008625aaecc95fb727a5fb346fc08984f
abhivanya <sbzabhishekyadav@gmail.com>
2024-05-25 11:23:01 +0530

    added Navigation

--- WheatherApp/App.js
+++ WheatherApp/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import { Pressable, View } from "react-native";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ header: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={SignupScreen} />
+        <Stack.Screen
+          name="Home"
+          options={{
+            header: true,
+            headerTitle: () => {
+              <View>
+                <Text>Wheather App</Text>
+              </View>;
+            },
+            headerRight: () => {
+              <Pressable>
+                <Text>Logout</Text>
+              </Pressable>;
+            },
+          }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ WheatherApp/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        LoginScreen
+      </Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
